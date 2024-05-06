51124900e977caf2cff963661565d659628561b4
bkty1122 <kwoktszyi1122@gmail.com>
2024-05-06 23:54:45 +0800

    upload

--- backend/backend.py
+++ backend/backend.py
@@ -3,11 +3,18 @@ from PIL import Image
 import numpy as np
 import joblib
 import io
+import tensorflow as tf
 
 app = Flask(__name__)
 
 # Load your trained model (ensure the path matches)
-model = joblib.load('model_resultfinal_stack_6.joblib')
+model = joblib.load('model_resultfinal_stack_2.joblib')
+
+# define the predict function
+def predict(features):
+    """ Make a prediction with the model """
+    prediction = model.predict([features])
+    return prediction
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -28,25 +35,35 @@ def predict():
             'changed': request.form['changed']
         }
 
-        # Convert form data to model input format
-        features = np.array([list(data.values())], dtype=float)
-
         # Check if an image file exists
         if 'image' in request.files:
             image_file = request.files['image']
             image = Image.open(image_file.stream)
-            # Example image processing: Convert to grayscale
-            image = image.convert('L')
-            image = image.resize((64, 64))  # Resize to the expected input size of the model
-            image_array = np.asarray(image).flatten()  # Flatten the image to 1D array
-            features = np.append(features, image_array)
-
-        # Make prediction
-        prediction = model.predict([features])
+            # Preprocess the image
+            image = img_preprocessing(image)
+            
+        # add the image to the data dictionary
+        data['image'] = image
+        
+        # Convert form data to model input format
+        features = np.array([list(data.values())], dtype=float)
+        
+        # make prediction
+        prediction = predict(features)
+        
         return jsonify({'prediction': str(prediction[0])})
         
     except Exception as e:
         return jsonify({'error': str(e)})
+
+# image processing for model
+def img_preprocessing(img):
+    """ Image preprocessing function """
+    img = tf.io.read_file(img)  # Read the image file
+    img = tf.image.decode_png(img, channels=3)  # Decode the PNG image
+    img = tf.image.resize(img, (256, 256))  # Resize the image, image size is (256, 256)
+    img = tf.cast(img, tf.float32) / 255.0  # Normalize pixel values to [0, 1] range
+    return img
 
 if __name__ == '__main__':
     app.run(debug=True)
