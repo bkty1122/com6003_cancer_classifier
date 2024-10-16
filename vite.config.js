cc1032861d240f15cb6cfd0b984ab690aaaa83b7
Kwok Tsz Yi <88106659+bkty1122@users.noreply.github.com>
2024-10-16 14:09:43 +0000

    update index

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/com6003_cancer_classifier.github.io/',
-});
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+})
